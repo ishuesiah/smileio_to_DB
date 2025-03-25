15a70f272e3587297b91febd6cf9f7229006c310
ishuesiah <129911713+ishuesiah@users.noreply.github.com>
2025-03-25 15:59:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- const express = require('express');
+const express = require('express');
 const mysql = require('mysql2/promise');
 const bodyParser = require('body-parser');
 require('dotenv').config();
